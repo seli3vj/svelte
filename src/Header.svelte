00e2ca95e696cf07f9d93b29813e1754cc5e962e
datacamp461 <data.camp461@audi.de>
2022-03-23 10:01:55 +0100

    Initial commit

---
+++ src/Header.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let teilnehmer;
+    console.log(teilnehmer)
+</script>
+
+<main>
+    <h1>This is the header {teilnehmer[0].first_name}</h1>
+</main>
+
+<style>
+
+</style>
---
+++ src/Teilnehmer.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {scale} from 'svelte/transition';
+export let name;
+
+</script>
+
+<main>
+    <div transition:scale class="karte">
+        {name}
+    </div>
+
+</main>
+
+
+<style>
+.karte {
+    background-color: lightgray;
+    border-radius: 20px;
+    margin: 1% 20%;
+    padding: 20px;
+    font-size: 2em
+}
+
+</style>
